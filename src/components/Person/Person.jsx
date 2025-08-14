e3479463b4a260d3438f3cecba89cba2d7705226
danie010830 <danielgukillwar008@gmail.com>
2025-08-14 15:48:42 -0300

    corrigido

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -9,7 +9,7 @@ export const Person = ({ person }) => {
       {person.isMarried ? (
         <p className="Person__partner">
           {person.partnerName} is my{' '}
-          {person.sex === 'male' ? 'wife' : 'husband'}
+          {person.sex === 'm' ? 'wife' : 'husband'}
         </p>
       ) : (
         <p className="Person__partner">I am not married</p>
